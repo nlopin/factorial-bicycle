879206c0ff51565ff215de6ec5bdf217c6253802
Nikolai Lopin <code@lopin.me>
2024-07-02 10:37:59 +0200

    add order placement

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,73 @@
-function test(a:number): number {
-    return a
+type FrameType = "FULL_SUSPENSION"| "DIAMOND"| "STEP_THROUGH"
+
+type FrameFinish = "MATTE" | "SHINY"
+
+type Wheels = "ROAD" | "MOUNTAIN" | "FAT_BIKE"
+
+type RimColor = "RED" | "BLACK" | "BLUE"
+
+type Chain = "SINGLE_SPEED" | "8_SPEED"
+
+type SharedOrderFields = {
+    frameType: FrameType,
+    frameFinish: FrameFinish,
+    wheels: Wheels
+    chain: Chain,
+    rimColor: RimColor
 }
 
-test(5)
+type UnverifiedOrder = SharedOrderFields & {
+    kind: "draft";
+}
+
+type PlacedOrder = SharedOrderFields & {
+    kind: "new"
+}
+
+type Either<R, E> = R | E
+
+type OrderError = string
+
+type ValidationResult = Either<[true], [false, string]>
+type ValidationFunction = (UnverifiedOrder) => ValidationResult
+
+function placeOrder(order: UnverifiedOrder): Either<PlacedOrder, OrderError> {
+    let error = null
+    const validations: ValidationFunction[]  = [checkMountainWheelsCombination, checkFatBikeWheelsCombination, checkAvailability]
+
+    const isVerified = validations.every(el => {
+        const [isOk, err] = el(order)
+        if (!isOk) error = err
+
+        return isOk
+    })
+
+    return isVerified ? {...order, kind: "new"} : error
+}
+
+function checkMountainWheelsCombination (order:UnverifiedOrder): ValidationResult {
+    if (order.wheels === "MOUNTAIN" && order.frameType !== "FULL_SUSPENSION") {
+        return [false, "MOUNTAIN_WHEELS_MUST_HAVE_FULL_SUSPENSION"]
+    }
+
+    return [true]
+}
+
+function checkFatBikeWheelsCombination (order:UnverifiedOrder): ValidationResult {
+    if (order.wheels === "FAT_BIKE" && order.rimColor === "RED") {
+        return [false, "FAT_BIKE_RED_RIM_NOT_POSSIBLE"]
+    }
+
+    return [true]
+}
+
+function checkAvailability (order: UnverifiedOrder): ValidationResult {
+    // @ts-ignore todo call DB
+    const isAvailable = getCombinationAvailability(order)
+
+    if (!isAvailable) {
+        return [false, "COMBINATION_NOT_IN_STOCK"]
+    }
+
+    return [true]
+}
